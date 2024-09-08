826cba0191f4b4cdb2d5140bbff30ea17f6b2be8
Andre-glitch-lang <IsidoIsido2012@gmail.com>
2024-09-08 15:05:15 +0300

    Add files via upload

---
+++ dinamika.py
@@ -0,0 +1,11 @@
+name = 'Andrey'
+print(name, type(name))
+name = 5
+print(name, type(name))
+name = 5.5
+print(name, type(name))
+name = [1, 2]
+print(name, type(name))
+a = 30
+new_age = 1
+print(a + 1)
